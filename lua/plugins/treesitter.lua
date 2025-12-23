eef177d450ce49a279e6a0d5feea13c35b94943d
segfaultchan <segfault@example.com>
2025-12-23 09:55:40 +0300

    update: fixed log errors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   build = ":TSUpdate",
-  main = 'nvim-treesitter.configs',
   opts = {
     auto_install = true,
     highlight = { enable = true },
