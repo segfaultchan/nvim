50eaa8978022c7396fe3f9e4263d9de5b98f7ec8
segfaultchan <segfault@example.com>
2025-12-29 17:48:28 +0300

    update: added bar and statusline

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,29 @@
+return {
+  { 'https://github.com/lewis6991/gitsigns.nvim/' },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      animation = false,
+      insert_at_start = true,
+      auto_hide = true,
+      
+      sidebar_filetypes = {
+        NvimTree = true,
+      },
+      
+    },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    init = function()
+      vim.opt.showmode = false -- disable modes for clear ui
+    end,
+
+    opts = {
+      icons_enabled = true,
+    }
+  }
+}
