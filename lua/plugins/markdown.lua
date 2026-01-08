e9d61d55e0be222d9175e1b01d26148238b42f7b
segfaultchan <segfaultchan@example.com>
2026-01-08 22:38:47 +0300

    fix: markdown didnt render

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,7 +3,6 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    lazy = true,
     opts = {},
     init = function()
       vim.api.nvim_create_autocmd("FileType", {
